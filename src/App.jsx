5dcae6a698dc30a25198179f05bd34f984ab916c
afeefa12 <85550119+afeefa12@users.noreply.github.com>
2025-04-07 16:38:27 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ShowPassword from './ShowPassword'
+
+function App() {
+  return (
+    <div>
+      <ShowPassword/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/ShowPassword.jsx
@@ -0,0 +1,42 @@
+// import React, { useState } from 'react'
+
+// const ShowPassword = () =>{
+//     const [show, setShow] = useState(false);
+
+//   return (
+//     <div>
+//         <input type ={show ? "text" : "password"} placeholder ="Enter password"/>
+//         <button onClick={()=> setShow(!show)}>
+//         {show ? "Hide" : "show"}
+//         </button>
+//     </div>
+//   );
+
+// };
+
+// export default ShowPassword
+
+
+import react,{useState} from 'react'
+const ShowPassword =() => {
+const [show, setShow] = useState(false)
+return(
+<div>
+    <input type = {show ? "text" : "password"} placeholder="Enter password"/>
+    <button onClick={() => setShow(!show)}>{show ? "Hide" : "show"}</button>
+</div>
+ );
+};
+ export default ShowPassword
+
+
+ //ternary operator
+ let age = 18;
+ let canVote = age >= 18 ? "yes" : "No";
+ console.log(canVote);
+//trim()
+"text ".trim()
+
+//padstart or padend
+ "5".padStart(3, "0")  //"005"
+ "5",.padEnd(3,"0")//"500"
